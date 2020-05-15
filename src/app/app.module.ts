eeff14881d84e9a86c5407192945e92ac4bbd9cb
Azeem Mirza <azeemrsd@gmail.com>
2020-05-15 18:25:12 +0400

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MenuComponent } from './menu/menu.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { TodoComponent } from './todo/todo.component';
+import { DiaryComponent } from './diary/diary.component';
 
+const appRoutes:Routes =[
+  { path:'' ,component: MenuComponent},
+  { path:'file-upload' ,component: FileUploadComponent},
+  { path:'todo' ,component: TodoComponent},
+  { path:'diary' ,component: DiaryComponent},
+]
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MenuComponent
+    MenuComponent,
+    TodoComponent,
+    DiaryComponent,
+    FileUploadComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
